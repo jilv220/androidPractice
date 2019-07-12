41834d08af9247b913ff54ad61901a616a0b818a
jm <lv.ji@jiamaintech.com>
2019-07-12 18:30:59 +0800

    image Rotation slow and buggy lol

--- app/src/main/java/com/example/myapplication/fragments/NotificationFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/NotificationFragment.kt
@@ -11,7 +11,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.graphics.scale
+import androidx.core.graphics.set
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
@@ -20,6 +23,7 @@ import com.example.myapplication.R
 import com.example.myapplication.databinding.Fragment3Binding
 import com.example.myapplication.fragmentsViewModel.NotificationFragmentViewModel
 import kotlinx.android.synthetic.main.fragment_3.*
+import kotlin.math.*
 
 val PICK_IMAGE = 7
 val PERMISSION_REQUEST_CODE = 8
@@ -30,6 +34,8 @@ class NotificationFragment : Fragment(),View.OnClickListener {
     var permissions : Array<String> = arrayOf(
             "android.permission.READ_EXTERNAL_STORAGE"
             ,"android.permission.WRITE_EXTERNAL_STORAGE")
+    var counter: Int = 0
+    lateinit var image : Bitmap
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -86,17 +92,85 @@ class NotificationFragment : Fragment(),View.OnClickListener {
 
     fun onRotateBtClick() {
 
-        var image : Bitmap = BitmapFactory.decodeResource(resources,R.id.iv_img)
+        if (counter == 0) {
+            image = readImgFromImageView(iv_img)
+        }
 
-        //rotate the image by 90 degrees
-        
+        var degree90 : Double = (90 * PI) / 180
+        var rotateConfig : Float = (((counter + 1) * degree90) % 360).toFloat()
 
+        var targetImage = roateImage(image,rotateConfig)
+        Log.e("target Image Size", "${targetImage.width} x ${targetImage.height}")
+
+        Glide.with(this)
+                .load(targetImage)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .placeholder(R.mipmap.ic_launcher_round)
+                .error(R.mipmap.ic_launcher_round)
+                .into(iv_img)
+
+        counter++
 
     }
 
     fun onScaleBtClick() {
 
     }
+
+    fun readImgFromImageView(view : ImageView) : Bitmap{
+
+        // enable the cache of the imageView
+        view.isDrawingCacheEnabled = true
+        var image : Bitmap = view.drawingCache
+
+        Toast.makeText(context,"图片读取完成",Toast.LENGTH_SHORT).show()
+
+        return image
+    }
+
+    //
+
+    fun roateImage (image : Bitmap, degree : Float) : Bitmap {
+
+        var width = image.width
+        var height = image.height
+
+        // Error
+        var errX : Float = 0f
+        var errY : Float = 0f
+
+        var fsin = sin(degree)
+        var fcos = cos(degree)
+
+        var newHeight : Int = ceil(abs(height * fcos) + abs(width * fsin)).toInt()
+        var newWidth : Int = ceil(abs(width * fcos) + abs(height * fsin)).toInt()
+
+        var targetImage :Bitmap = BitmapFactory.decodeResource(resources,R.mipmap.timg).scale(newWidth,newHeight)
+
+        var dstX = (width - newWidth * fcos - newHeight * fsin) / 2
+        var dstY = (height + newWidth * fsin - newHeight * fcos) / 2
+
+        for (x in 0..newHeight) {
+            for (y in 0..newWidth) {
+
+                var reverseX = x*fcos + y*fsin + dstX
+                var reverseY = y*fcos - x*fsin + dstY
+
+                if (reverseX > width - 1 || reverseX < 0 || reverseY > height - 1 || reverseY < 0) {
+
+
+                } else {
+
+                    targetImage[x,y] = image.getPixel(reverseX.toInt(),reverseY.toInt())
+                }
+
+            }
+        }
+
+        return targetImage
+
+    }
+
 
 
 }
